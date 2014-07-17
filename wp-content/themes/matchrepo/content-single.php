76b4572fa9077aa53bc01afe8c51248e3f3e7b0f
cobi zaltzberg <jzaltzberg@gmail.com>
2014-07-17 12:46:46 +0300

    single

--- wp-content/themes/matchrepo/content-single.php
+++ wp-content/themes/matchrepo/content-single.php
@@ -1,8 +1,29 @@
 <?
+
+/** @var WP_Post $post */
+
 $arr_post = (array) $post;
+
 $gender = CardHelpers::get_gender($arr_post);
+
 $MCard = new $gender($arr_post);
+
 $id = get_the_ID();
+
+$contact = get_user_by('id', $post -> post_author);
+
+$contact_meta = get_user_meta($post -> post_author);
+
+$contactItems = ['user_type', 'user_phone'];
+
+foreach($contactItems as $item)
+	$$item = ! empty($contact_meta[$item][0]) ? $contact_meta[$item][0] : null;
+
+$cardManager = get_the_author();
+
+if(! empty( Cards::$user_types[$user_type]))
+	$cardManager .= ', ' . Cards::$user_types[$user_type];
+
 ?>
 
 <article id="post-<? $id ?>" class="background-area">
@@ -23,11 +44,27 @@ $id = get_the_ID();
 			</div>
 			<div class="card-side-box">
 				<div class="title"><?= stripslashes(__('This Card Managed By', THEME_NAME)) ?></div>
-				<div class="cdb-content"></div>
+				<div id="card-manager" class="cdb-content"><?= $cardManager ?></div>
 			</div>
 			<div class="card-side-box">
 				<div class="title"><? _e('Contact Details', THEME_NAME) ?></div>
-				<div class="cdb-content"></div>
+				<div class="cdb-content">
+					<? if($user_phone){ ?>
+						<div class="row">
+							<div class="label">טלפון:</div>
+							<div class="term">
+								<? $phone = Matchrepo::phone_format($user_phone) ?>
+								<a href="tel:<?= $phone ?>"><?= $phone ?></a>
+							</div>
+						</div>
+					<? } ?>
+					<div class="row">
+						<div class="label">דוא"ל:</div>
+						<div class="term">
+							<a target="_blank" href="mailto:<?= $contact -> user_email ?>"><?= $contact -> user_email ?></a>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
