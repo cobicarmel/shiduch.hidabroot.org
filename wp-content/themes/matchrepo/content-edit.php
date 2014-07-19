651b59dbc2434781ad0d26e378c967cb04e27202
cobi zaltzberg <jzaltzberg@gmail.com>
2014-07-19 21:28:34 +0300

    המשך עבודה על עריכת כרטיס

--- wp-content/themes/matchrepo/content-edit.php
+++ wp-content/themes/matchrepo/content-edit.php
@@ -7,10 +7,13 @@ $gender = CardHelpers::get_gender($arr_post);
 
 $MCard = new $gender($arr_post);
 
-$meta = $MCard -> get_meta();
+$meta = $MCard ->get_meta();
+
+$props = $gender::$props;
 ?>
 
 <form id="main-form" method="post">
+	<input type="hidden" name="gender" value="<?= $gender == 'male' ? 0 : 1 ?>">
 
 	<div id="mf-options">
 		<div class="row">
@@ -20,23 +23,22 @@ $meta = $MCard -> get_meta();
 			</div>
 			<div class="label-top w33">
 				<label for="cf-birthday">תאריך לידה</label>
-				<input id="cf-birthday" type="text" name="birthday" value="<?= $meta['birthday'] ?>" required>
+				<input id="cf-birthday" type="text" name="birthday"
+					   value="<?= date('d/m/Y', strtotime($meta['birthday'])) ?>" required>
 			</div>
 			<div class="label-top w16">
 				<label for="cf-status">מצב משפחתי</label>
 				<select id="cf-status" class="toggle-trigger show-hide-trigger" name="status"
 						data-toggle-key="children" required>
 					<option></option>
-					<option value="0"><?= $props['status']['options'][0] ?></option>
-					<option value="1"><?= $props['status']['options'][1] ?></option>
-					<option value="2"><?= $props['status']['options'][2] ?></option>
+					<? Matchrepo::listOptions($props['status']['options'], $meta['status']) ?>
 				</select>
 			</div>
-			<div class="label-top w16 toggle-affected show-hide-affected" data-affected="children"
-				 style="display: none">
+			<div class="label-top w16 toggle-affected show-hide-affected"
+				 data-affected="children"<?= $meta['status'] ? '' : ' style="display: none"' ?>>
 				<label for="cf-children">מספר ילדים</label>
 				<select id="cf-children" name="children">
-					<? foreach(range(0, 20) as $number){ ?>
+					<? foreach(range(0, 20) as $number) { ?>
 						<option><?= $number ?></option>
 					<? } ?>
 				</select>
@@ -53,18 +55,14 @@ $meta = $MCard -> get_meta();
 				<label for="cf-zone">איזור מגורים</label>
 				<select id="cf-zone" name="zone" required>
 					<option></option>
-					<? foreach($props['zone']['options'] as $zone){ ?>
-						<option><?= $zone ?></option>
-					<? } ?>
+					<? Matchrepo::listOptions($props['zone']['options'], $meta['zone'], true) ?>
 				</select>
 			</div>
 			<div class="label-top w33">
 				<label for="cf-city">עיר מגורים</label>
 				<select id="cf-city" name="city" required>
 					<option></option>
-					<? foreach($props['city']['options'] as $city){ ?>
-						<option><?= $city ?></option>
-					<? } ?>
+					<? Matchrepo::listOptions($props['city']['options'], $meta['city'], true) ?>
 				</select>
 			</div>
 		</div>
@@ -73,50 +71,43 @@ $meta = $MCard -> get_meta();
 				<label for="cf-community">מוצא עדתי</label>
 				<select id="cf-community" name="community" required>
 					<option></option>
-					<? $communities = $props['community']['options']; ?>
-					<option value="0"><?= $communities[0] ?></option>
-					<option value="1"><?= $communities[1] ?></option>
-					<option value="2"><?= $communities[2] ?></option>
+					<? Matchrepo::listOptions($props['community']['options'], $meta['community']) ?>
 				</select>
 			</div>
 			<div class="label-top w25">
 				<label for="cf-conception">השקפה</label>
 				<select id="cf-conception" class="toggle-trigger hasidism-trigger" name="conception" required>
 					<option></option>
-					<? $conceptions = $props['conception']['options']; ?>
-					<option value="0"><?= $conceptions[0] ?></option>
-					<option value="1"><?= $conceptions[1] ?></option>
-					<option value="2"><?= $conceptions[2] ?></option>
+					<? Matchrepo::listOptions($props['conception']['options'], $meta['conception']) ?>
 				</select>
 			</div>
-			<div class="label-top w25 toggle-affected hasidism-affected" style="display: none">
+			<div
+				class="label-top w25 toggle-affected hasidism-affected"<?= $meta['conception'] != 2 ? ' style="display: none"' : '' ?>>
 				<label for="cf-hasidism">חסידות</label>
-				<select id="cf-hasidism" name="hasidism" required>
+				<select id="cf-hasidism" name="hasidism" required<?= $meta['conception'] != 2 ? ' disabled' : '' ?>>
 					<option></option>
-					<? foreach($props['hasidism']['options'] as $hasidut){ ?>
-						<option><?= $hasidut ?></option>
-					<? } ?>
+					<? Matchrepo::listOptions($props['hasidism']['options'], $meta['hasidism'], true) ?>
 				</select>
 			</div>
 		</div>
 		<div class="row">
 			<div class="label-top w33">
 				<label for="cf-work">מקום לימודים/עיסוק כיום</label>
-				<input id="cf-work" type="text" name="work" required>
+				<input id="cf-work" type="text" name="work" value="<?= $meta['work'] ?>" required>
 			</div>
 			<div class="label-top w33">
 				<label for="cf-college">לימודים בעבר</label>
-				<input id="cf-college" type="text" name="college" required>
+				<input id="cf-college" type="text" name="college" value="<?= $meta['college'] ?>" required>
 			</div>
 		</div>
 		<div class="row">
 			<div class="label-top w33">
 				<label for="cf-father-work">עיסוק האב</label>
-				<input id="cf-father-work" type="text" name="father_work" required>
+				<input id="cf-father-work" type="text" name="father_work" value="<?= $meta['father_work'] ?>" required>
 			</div>
 			<div class="label-top w33">
 				<label for="cf-mother-work">עיסוק האם</label>
-				<input id="cf-mother-work" type="text" name="mother_work" required>
+				<input id="cf-mother-work" type="text" name="mother_work" value="<?= $meta['mother_work'] ?>" required>
 			</div>
 		</div>
 		<div class="row">
@@ -124,9 +115,7 @@ $meta = $MCard -> get_meta();
 				<label for="cf-height">גובה</label>
 				<select id="cf-height" name="height" required>
 					<option></option>
-					<? foreach(range(120, 210) as $height){ ?>
-						<option><?= $height ?></option>
-					<? } ?>
+					<? Matchrepo::listOptions(range(120, 210), $meta['height'], true) ?>
 				</select>
 			</div>
 		</div>
@@ -135,29 +124,25 @@ $meta = $MCard -> get_meta();
 				<label for="cf-look">מראה כללי</label>
 				<select id="cf-look" name="look">
 					<option></option>
-					<? foreach($props['look']['options'] as $i => $look){ ?>
-						<option value="<?= $i ?>"><?= $look ?></option>
-					<? } ?>
+					<? Matchrepo::listOptions($props['look']['options'], $meta['look']) ?>
 				</select>
 			</div>
 		</div>
 		<div class="row">
-			<? if($gender){ ?>
+			<? if($gender == 'female') { ?>
 				<div class="label-top w25">
 					<label for="cf-cover">כיסוי ראש</label>
 					<select id="cf-cover" name="cover">
 						<option></option>
-						<? foreach($props['cover']['options'] as $i => $cover){ ?>
-							<option value="<?= $i ?>"><?= $cover ?></option>
-						<? } ?>
+						<? Matchrepo::listOptions($props['cover']['options'], $meta['cover']) ?>
 					</select>
 				</div>
 			<?
 			}
-			else{
+			else {
 				?>
 				<div class="w25">
-					<input type="checkbox" id="cf-smoke" name="smoke">
+					<input type="checkbox" id="cf-smoke" name="smoke" value="1" <? checked($meta['smoke']) ?>>
 					<label for="cf-smoke">מעשן</label>
 				</div>
 			<? } ?>
@@ -168,35 +153,35 @@ $meta = $MCard -> get_meta();
 				<select id="cf-healthy" class="toggle-trigger show-hide-trigger" data-toggle-key="healthy"
 						name="healthy">
 					<option></option>
-					<? foreach($props['healthy']['options'] as $i => $healthy){ ?>
-						<option value="<?= $i ?>"><?= $healthy ?></option>
-					<? } ?>
+					<? Matchrepo::listOptions($props['healthy']['options'], $meta['healthy']) ?>
 				</select>
 			</div>
 		</div>
 		<div class="row">
-			<div id="cf-disability" class="toggle-affected show-hide-affected" data-affected="healthy">
+			<div id="cf-disability" class="toggle-affected show-hide-affected"
+				 data-affected="healthy"<?= $meta['healthy'] ? : ' style="display: none"' ?>>
 				<div>פירוט מוגבלות</div>
-				<? foreach($props['disability_details']['options'] as $i => $disability){ ?>
-					<div>
-						<input type="checkbox" name="disability_details[]" id="cf-disability<?= $i ?>"
-							   value="<?= $i ?>">
-						<label for="cf-disability<?= $i ?>"><?= $disability ?></label>
-					</div>
-				<? } ?>
+				<?
+
+				$options = ['id' => 'cf-disability', 'name' => 'disability_details', 'compare' => $meta['disability_details']];
+
+				Matchrepo::listCheckboxes($props['disability_details']['options'], $options);
+
+				?>
 				<div>
 					<input type="checkbox" id="cf-disability-other">
 					<label for="cf-disability-other">אחר - נא לפרט:</label>
-					<textarea name="disability_details[]"></textarea>
+					<textarea id="cf-disability-other"
+							  name="other_disability"><?= $meta['other_disability'] ?></textarea>
 				</div>
 			</div>
 		</div>
 		<div class="row">
-			<label for="cf-content"><?= $labels['Little_About_The_Candidate'] ?>:</label>
-			<textarea id="cf-content" name="content" required></textarea>
+			<label for="cf-content"><?= $MCard::$labels['Little_About_The_Candidate'] ?>:</label>
+			<textarea id="cf-content" name="content" required><?= strip_tags(get_the_content()) ?></textarea>
 		</div>
 	</div>
 	<div id="submit">
-		<input type="submit" value="הוספת כרטיס">
+		<input type="submit" value="שמירת שינויים">
 	</div>
 </form>
--- wp-content/themes/matchrepo/edit-card.php
+++ wp-content/themes/matchrepo/edit-card.php
@@ -4,43 +4,72 @@
 Template Name: עריכת כרטיס
 */
 
-if(! current_user_can('edit_posts', $_GET['id']))
+$post_id = $_GET['id'];
+
+if(! current_user_can('edit_posts', $post_id))
 	wp_die('לך הביתה, פעם אחרונה שאתה עושה כאלו שטויות!');
 
 Matchrepo::mainFormHeader();
 
 Matchrepo::cardFormHeader();
 
+if($_POST) {
+
+	$isCorrect = Cards::validateCardData($_POST);
+
+	if($isCorrect === true) {
+
+		wp_update_post([
+			'ID' => $post_id,
+			'post_title' => $_POST['title'],
+			'post_content' => $_POST['content']
+		]);
+
+		unset($_POST['title'], $_POST['content']);
+
+		$_POST['birthday'] = Matchrepo::textToDBDate($_POST['birthday']);
+
+		$cardTerms = Cards::getTerms();
+
+		foreach($cardTerms as $term)
+			update_post_meta($post_id, $term, isset($_POST[$term]) ? $_POST[$term] : '');
+	}
+	else{
+		var_dump($isCorrect);
+		die;
+	}
+}
+
 get_header();
 
-$args = array(
+$args = [
 	'post_type' => 'card',
 	'posts_per_page' => 1,
-	'post__in' => [$_GET['id']]
-);
+	'post__in' => [$post_id]
+];
 
 query_posts($args);
 
 ?>
 
-<div id="primary" class="content-area">
-	<main id="main" class="site-main" role="main">
-		<div class="background-area">
-			<?
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+			<div class="background-area">
+				<?
 
-			while(have_posts()) :
+				while(have_posts()) :
 
-				the_post();
+					the_post();
 
-				get_template_part('content', 'edit');
+					get_template_part('content', 'edit');
 
-			endwhile;
+				endwhile;
 
-			?>
-		</div>
-	</main>
-	<!-- #main -->
-</div><!-- #primary -->
+				?>
+			</div>
+		</main>
+		<!-- #main -->
+	</div><!-- #primary -->
 
 <? get_sidebar() ?>
 <? get_footer() ?>
