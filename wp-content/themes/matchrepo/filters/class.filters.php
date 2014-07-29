d676dd918d39828e9bc73be97b50b31f9d54235f
cobi zaltzberg <jzaltzberg@gmail.com>
2014-07-29 04:39:30 +0300

    comment

--- wp-content/themes/matchrepo/filters/class.filters.php
+++ wp-content/themes/matchrepo/filters/class.filters.php
@@ -31,6 +31,8 @@ add_filter('login_headerurl', 'MR_filters::login_logo_url');
 
 add_filter('login_headertitle', 'MR_filters::login_logo_title');
 
+/* Mail filters */
+
 add_filter('wp_mail_from', 'MR_filters::custom_mail_from');
 
 add_filter('wp_mail_from_name', 'MR_filters::custom_mail_from_name');
