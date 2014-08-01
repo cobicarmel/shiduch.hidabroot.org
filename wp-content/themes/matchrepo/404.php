0075a5976a82e04b8ab6cb1a9b487e0400fd9fc4
cobi zaltzberg <jzaltzberg@gmail.com>
2014-08-01 04:16:16 +0300

    404

--- wp-content/themes/matchrepo/404.php
+++ wp-content/themes/matchrepo/404.php
@@ -1,5 +1,7 @@
 <?
 
+wp_enqueue_style('404', get_stylesheet_directory_uri() . '/css/404.css');
+
 get_header(); ?>
 
 	<div id="primary" class="content-area">
@@ -7,18 +9,11 @@ get_header(); ?>
 
 			<section class="error-404 not-found">
 				<header class="page-header">
-					<h1 class="page-title"><? _e( 'Oops! That page can&rsquo;t be found.', THEME_NAME ); ?></h1>
-				</header><!-- .page-header -->
+					<div><? _e( 'Oops! That page can&rsquo;t be found.', THEME_NAME ); ?></div>
+				</header>
+			</section>
 
-				<div class="page-content">
-					<p><? _e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', THEME_NAME ); ?></p>
-
-					<? get_search_form(); ?>
-
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+		</main>
+	</div>
 <? get_sidebar() ?>
 <? get_footer(); ?>
