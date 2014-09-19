0afc2ee6e2b9fcd0ce94982e8a4277a7a54353f2
cobi zaltzberg <jzaltzberg@gmail.com>
2014-09-19 03:35:14 +0300

    redundant things removed from wp-config

--- wp-config.php
+++ wp-config.php
@@ -1,18 +1,4 @@
-<?php // Modified for Hebrew translation
-/**
- * The base configurations of the WordPress.
- *
- * This file has the following configurations: MySQL settings, Table Prefix,
- * Secret Keys, WordPress Language, and ABSPATH. You can find more information
- * by visiting {@link http://codex.wordpress.org/Editing_wp-config.php Editing
- * wp-config.php} Codex page. You can get the MySQL settings from your web host.
- *
- * This file is used by the wp-config.php creation script during the
- * installation. You don't have to use the web site, you can just copy this file
- * to "wp-config.php" and fill in the values.
- *
- * @package WordPress
- */
+<?
 
 define('IS_LOCAL', $_SERVER['HTTP_HOST'] == 'localhost');
 
@@ -47,15 +33,6 @@ define('DB_COLLATE', '');
 define('WPCF7_LOAD_CSS', false);
 
 define('WPCF7_LOAD_JS', false);
-
-define('AUTH_KEY',         'put your unique phrase here');
-define('SECURE_AUTH_KEY',  'put your unique phrase here');
-define('LOGGED_IN_KEY',    'put your unique phrase here');
-define('NONCE_KEY',        'put your unique phrase here');
-define('AUTH_SALT',        'put your unique phrase here');
-define('SECURE_AUTH_SALT', 'put your unique phrase here');
-define('LOGGED_IN_SALT',   'put your unique phrase here');
-define('NONCE_SALT',       'put your unique phrase here');
 
 $table_prefix  = 'wp_';
 
