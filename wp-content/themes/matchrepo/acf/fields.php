73024e7d292fb31d5cd3ed363a6914d418038368
cobi zaltzberg <jzaltzberg@gmail.com>
2014-09-23 02:20:08 +0300

    הסרת שדה "פרטים מוסתרים"

--- wp-content/themes/matchrepo/acf/fields.php
+++ wp-content/themes/matchrepo/acf/fields.php
@@ -698,17 +698,6 @@ if(function_exists("register_field_group"))
 				'default_value' => '',
 				'allow_null' => 0,
 				'multiple' => 0,
-			),
-			array (
-				'key' => 'field_53f400104e359',
-				'label' => 'פרטים מוסתרים',
-				'name' => 'hidden_details',
-				'type' => 'textarea',
-				'default_value' => '',
-				'placeholder' => '',
-				'maxlength' => '',
-				'rows' => '',
-				'formatting' => 'none',
 			)
 		),
 		'location' => array (
