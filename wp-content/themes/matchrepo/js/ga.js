e01078deebd51fc687392dd7e956dd78f791230b
cobi zaltzberg <jzaltzberg@gmail.com>
2014-09-28 01:45:29 +0300

    google analytics

--- wp-content/themes/matchrepo/js/ga.js
+++ wp-content/themes/matchrepo/js/ga.js
@@ -2,8 +2,6 @@
 	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-var pluginUrl =
-	'//www.google-analytics.com/plugins/ga/inpage_linkid.js';
 
 ga('require', 'linkid', 'linkid.js');
 ga('create', 'UA-55213286-1', 'auto');
