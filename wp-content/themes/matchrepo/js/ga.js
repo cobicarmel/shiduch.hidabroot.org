fcf89a8e2caa4905adfabdf046982acf0318ca7f
cobi zaltzberg <jzaltzberg@gmail.com>
2014-09-28 01:21:45 +0300

    google analytics

--- wp-content/themes/matchrepo/js/ga.js
+++ wp-content/themes/matchrepo/js/ga.js
@@ -3,5 +3,6 @@
 	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
+ga('require', 'linkid', 'linkid.js');
 ga('create', 'UA-55213286-1', 'auto');
 ga('send', 'pageview');
